2ea9cd289f17013ccda8a3eefeb0563bcb0fcd65
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-10 12:13:46 -0300

    chore: add and implement dotenv morgan and cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import { connectDatabases } from "./utilities";
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.send("Cambalache REST");
 });
 
-app.listen(3000, () => console.log("Server init"));
+connectDatabases()
+  .then(() => app.listen(3000, () => console.log("Server init")))
+  .catch((err) => console.error("Error: " + err));
