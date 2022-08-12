f0399c7dbae458ca8159185a8cdf904660ed7515
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-12 10:51:11 -0300

    feat: implement documentation

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
 
+import documentation from "./docs";
 import { connectDatabases } from "./utilities";
 
 const app = express();
@@ -16,6 +18,8 @@ app.use(cors());
 app.get("/", (_req, res) => {
   res.send("Cambalache REST");
 });
+
+app.use("/documentation", swaggerUI.serve, swaggerUI.setup(documentation));
 
 connectDatabases()
   .then(() => app.listen(3000, () => console.log("Server init")))
