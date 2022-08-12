4971cdfff878f1609a19876fa94b066c36fd2832
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-12 10:48:42 -0300

    feat: add validator messages error

---
+++ src/config/validator-error-messages.ts
@@ -0,0 +1,10 @@
+const errorMessages = {
+  isRequired: "is required",
+  isString: "must be an string",
+  isEmail: "must be a valid email",
+  isDate: "must be a valid date",
+  isNumeric: "must be a number",
+  notEmpty: "must not be empty",
+};
+
+export default errorMessages;
